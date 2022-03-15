e0b6d95a01f5dcde0147edb65f671953b52dcfda
Anıl Can <71456529+ancalpar@users.noreply.github.com>
2022-03-15 09:39:04 +0300

    .

--- 1. Basic Select/10. Weather Observation Station 6.sql
+++ 1. Basic Select/10. Weather Observation Station 6.sql
@@ -1,4 +1,4 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+-- https://www.hackerrank.com/challenges/weather-observation-station-6/problem 
 
 SELECT DISTINCT CITY
 FROM STATION
